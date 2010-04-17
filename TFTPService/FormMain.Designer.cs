630a91f06d81466c840cde8b4395d5339430980a
Jean-Paul Mikkers <jean.paul.mikkers@gmail.com>
2010-04-17 22:22:59 +0000

    Added Setup project. Rewrite to windows service.

---
+++ TFTPService/FormMain.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace TFTPService
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonElevate = new System.Windows.Forms.Button();
+            this.timerServiceWatcher = new System.Windows.Forms.Timer(this.components);
+            this.buttonConfigure = new System.Windows.Forms.Button();
+            this.buttonAbout = new System.Windows.Forms.Button();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonHistoryOneDay = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.buttonHistoryAll = new System.Windows.Forms.Button();
+            this.labelFilter = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonHistoryOneHour = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(8, 12);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "&Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(89, 12);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 1;
+            this.buttonStop.Text = "S&top";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonElevate
+            // 
+            this.buttonElevate.Location = new System.Drawing.Point(251, 12);
+            this.buttonElevate.Name = "buttonElevate";
+            this.buttonElevate.Size = new System.Drawing.Size(75, 23);
+            this.buttonElevate.TabIndex = 3;
+            this.buttonElevate.Text = "&Elevate";
+            this.buttonElevate.UseVisualStyleBackColor = true;
+            this.buttonElevate.Click += new System.EventHandler(this.buttonElevate_Click);
+            // 
+            // timerServiceWatcher
+            // 
+            this.timerServiceWatcher.Enabled = true;
+            this.timerServiceWatcher.Interval = 1000;
+            this.timerServiceWatcher.Tick += new System.EventHandler(this.timerServiceWatcher_Tick);
+            // 
+            // buttonConfigure
+            // 
+            this.buttonConfigure.Location = new System.Drawing.Point(170, 12);
+            this.buttonConfigure.Name = "buttonConfigure";
+            this.buttonConfigure.Size = new System.Drawing.Size(75, 23);
+            this.buttonConfigure.TabIndex = 2;
+            this.buttonConfigure.Text = "&Configure";
+            this.buttonConfigure.UseVisualStyleBackColor = true;
+            this.buttonConfigure.Click += new System.EventHandler(this.buttonConfigure_Click);
+            // 
+            // buttonAbout
+            // 
+            this.buttonAbout.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonAbout.Location = new System.Drawing.Point(511, 7);
+            this.buttonAbout.Name = "buttonAbout";
+            this.buttonAbout.Size = new System.Drawing.Size(63, 32);
+            this.buttonAbout.TabIndex = 4;
+            this.buttonAbout.Text = "About";
+            this.buttonAbout.UseVisualStyleBackColor = true;
+            this.buttonAbout.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.EnableRaisingEvents = true;
+            this.eventLog1.Log = "TFTPServer";
+            this.eventLog1.Source = "TFTPServer";
+            this.eventLog1.SynchronizingObject = this;
+            this.eventLog1.EntryWritten += new System.Diagnostics.EntryWrittenEventHandler(this.eventLog1_EntryWritten);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(8, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(566, 215);
+            this.textBox1.TabIndex = 0;
+            // 
+            // buttonHistoryOneDay
+            // 
+            this.buttonHistoryOneDay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonHistoryOneDay.Location = new System.Drawing.Point(391, 9);
+            this.buttonHistoryOneDay.Name = "buttonHistoryOneDay";
+            this.buttonHistoryOneDay.Size = new System.Drawing.Size(57, 23);
+            this.buttonHistoryOneDay.TabIndex = 3;
+            this.buttonHistoryOneDay.Text = "+1 day";
+            this.buttonHistoryOneDay.UseVisualStyleBackColor = true;
+            this.buttonHistoryOneDay.Click += new System.EventHandler(this.buttonShowHistory_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClear.Location = new System.Drawing.Point(517, 9);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(57, 23);
+            this.buttonClear.TabIndex = 5;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonStart);
+            this.panel1.Controls.Add(this.buttonStop);
+            this.panel1.Controls.Add(this.buttonAbout);
+            this.panel1.Controls.Add(this.buttonConfigure);
+            this.panel1.Controls.Add(this.buttonElevate);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(582, 46);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.labelFilter);
+            this.panel2.Controls.Add(this.buttonHistoryAll);
+            this.panel2.Controls.Add(this.buttonHistoryOneHour);
+            this.panel2.Controls.Add(this.buttonHistoryOneDay);
+            this.panel2.Controls.Add(this.buttonClear);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 261);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(582, 41);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.textBox1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 46);
+            this.panel3.Name = "panel3";
+            this.panel3.Padding = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.panel3.Size = new System.Drawing.Size(582, 215);
+            this.panel3.TabIndex = 1;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 302);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(582, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(93, 17);
+            this.toolStripStatusLabel.Text = "Service Status : -";
+            // 
+            // buttonHistoryAll
+            // 
+            this.buttonHistoryAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonHistoryAll.Location = new System.Drawing.Point(454, 9);
+            this.buttonHistoryAll.Name = "buttonHistoryAll";
+            this.buttonHistoryAll.Size = new System.Drawing.Size(57, 23);
+            this.buttonHistoryAll.TabIndex = 4;
+            this.buttonHistoryAll.Text = "All";
+            this.buttonHistoryAll.UseVisualStyleBackColor = true;
+            this.buttonHistoryAll.Click += new System.EventHandler(this.buttonHistoryAll_Click);
+            // 
+            // labelFilter
+            // 
+            this.labelFilter.AutoSize = true;
+            this.labelFilter.Location = new System.Drawing.Point(5, 14);
+            this.labelFilter.Name = "labelFilter";
+            this.labelFilter.Size = new System.Drawing.Size(117, 13);
+            this.labelFilter.TabIndex = 0;
+            this.labelFilter.Text = "Showing log starting at:";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(255, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "More history:";
+            // 
+            // buttonHistoryOneHour
+            // 
+            this.buttonHistoryOneHour.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonHistoryOneHour.Location = new System.Drawing.Point(328, 9);
+            this.buttonHistoryOneHour.Name = "buttonHistoryOneHour";
+            this.buttonHistoryOneHour.Size = new System.Drawing.Size(57, 23);
+            this.buttonHistoryOneHour.TabIndex = 2;
+            this.buttonHistoryOneHour.Text = "+1 hour";
+            this.buttonHistoryOneHour.UseVisualStyleBackColor = true;
+            this.buttonHistoryOneHour.Click += new System.EventHandler(this.buttonHistoryOneHour_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 324);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.statusStrip1);
+            this.MinimumSize = new System.Drawing.Size(420, 200);
+            this.Name = "Form1";
+            this.Text = "TFTP Service";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonElevate;
+        private System.Windows.Forms.Timer timerServiceWatcher;
+        private System.Windows.Forms.Button buttonConfigure;
+        private System.Windows.Forms.Button buttonAbout;
+        private System.Diagnostics.EventLog eventLog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonHistoryOneDay;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.Label labelFilter;
+        private System.Windows.Forms.Button buttonHistoryAll;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonHistoryOneHour;
+    }
+}
---
+++ TFTPService/TFTPService.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Configuration;
+using System.Threading;
+using CodePlex.JPMikkers.TFTP;
+
+namespace TFTPService
+{
+    public partial class TFTPService : ServiceBase
+    {
+        private TFTPServerConfigurationList m_Configuration;
+        private List<TFTPServer> m_Servers;
+
+        public TFTPService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            m_Configuration = TFTPServerConfigurationList.Read(Program.GetConfigurationPath());
+            m_Servers = new List<TFTPServer>();
+
+            foreach (var config in m_Configuration)
+            {
+                TFTPServer server = new TFTPServer();
+                server.EndPoint = config.EndPoint;
+                server.SinglePort = config.SinglePort;
+                server.Ttl = (short)config.Ttl;
+                server.DontFragment = config.DontFragment;
+                server.RootPath = config.RootPath;
+                server.AutoCreateDirectories = config.AutoCreateDirectories;
+                server.AllowRead = config.AllowRead;
+                server.AllowWrite = config.AllowWrite;
+                server.ResponseTimeout = config.Timeout;
+                server.Retries = config.Retries;
+                server.ConvertPathSeparator = config.ConvertPathSeparator;
+                server.OnStatusChange += server_OnStatusChange;
+                server.OnTrace += server_OnTrace;
+                m_Servers.Add(server);
+            }
+
+            foreach (var server in m_Servers)
+            {
+                try
+                {
+                    server.Start();
+                }
+                catch (Exception e)
+                {
+                    server.OnStatusChange -= server_OnStatusChange;
+                    server.OnTrace -= server_OnTrace;
+                }
+            }
+        }
+
+        private void server_OnTrace(object sender, TFTPTraceEventArgs e)
+        {
+            //System.Diagnostics.Debug.WriteLine(e.Message);
+            eventLog1.WriteEntry(e.Message);
+        }
+
+        private void server_OnStatusChange(object sender, TFTPStopEventArgs e)
+        {
+            TFTPServer server = (TFTPServer)sender;
+            //System.Diagnostics.Debug.WriteLine(string.Format("{0}, {1} transfers", server.Active ? "Active" : "Stopped", server.ActiveTransfers));
+            eventLog1.WriteEntry(string.Format("{0}, {1} transfers", server.Active ? "Active" : "Stopped", server.ActiveTransfers));
+        }
+
+        protected override void OnStop()
+        {
+            foreach (var server in m_Servers)
+            {
+                server.Stop();
+                server.OnStatusChange -= server_OnStatusChange;
+                server.OnTrace -= server_OnTrace;
+            }
+            m_Servers.Clear();
+        }
+
+        protected override void OnCustomCommand(int command)
+        {
+            base.OnCustomCommand(command);
+        }
+    }
+}
