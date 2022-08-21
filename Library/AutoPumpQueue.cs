68dc0b54423f1f481a0f86c2d7fee49d3749d08f
jpmikkers <jean.paul.mikkers@gmail.com>
2022-08-21 15:05:14 +0200

    Use ConcurrentQueue in AutoPumpQueue

--- Library/AutoPumpQueue.cs
+++ Library/AutoPumpQueue.cs
@@ -1,14 +1,12 @@
 ﻿namespace Baksteen.Net.TFTP.Server;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Threading;
 
 public class AutoPumpQueue<T>
 {
     public delegate void DataDelegate(AutoPumpQueue<T> sender, T data);
-
-    private readonly object _queueSync = new object();
-    private readonly object _dispatchSync = new object();
-    private readonly Queue<T> _queue;
+    private readonly object _dispatchSync = new();
+    private readonly ConcurrentQueue<T> _queue;
     private readonly DataDelegate _dataDelegate;
 
     /// <summary>
@@ -16,7 +14,7 @@ public class AutoPumpQueue<T>
     /// </summary>
     public AutoPumpQueue(DataDelegate dataDelegate)
     {
-        _queue = new Queue<T>();
+        _queue = new ConcurrentQueue<T>();
         _dataDelegate = dataDelegate;
     }
 
@@ -24,35 +22,14 @@ public class AutoPumpQueue<T>
     {
         lock(_dispatchSync)    // ensures individual invokes are serialized
         {
-            bool empty = false;
-            T data = default(T);
-
-            while(!empty)
+            while(_queue.TryDequeue(out var data))
             {
-                lock(_queueSync)
+                try
                 {
-                    if(_queue.Count == 0)
-                    {
-                        // no data
-                        empty = true;
-                    }
-                    else
-                    {
-                        // there are commands;
-                        data = _queue.Dequeue();
-                        empty = false;
-                    }
+                    _dataDelegate(this, data);
                 }
-
-                if(!empty)
+                catch
                 {
-                    try
-                    {
-                        _dataDelegate(this, data);
-                    }
-                    catch
-                    {
-                    }
                 }
             }
         }
@@ -60,13 +37,8 @@ public class AutoPumpQueue<T>
 
     public void Enqueue(T data)
     {
-        bool queueWasEmpty;
-
-        lock(_queueSync)
-        {
-            queueWasEmpty = (_queue.Count == 0);
-            _queue.Enqueue(data);
-        }
+        bool queueWasEmpty = _queue.IsEmpty;
+        _queue.Enqueue(data);
 
         if(queueWasEmpty)
         {
