9a616f8648136fb4e1efea8f18f9cbe1e8533b7e
jpmikkers <jean.paul.mikkers@gmail.com>
2024-03-11 16:20:41 +0100

    fix dialog focusing problems

--- AvaTFTPServer/Services/TFTPAppDialogsImpl.cs
+++ AvaTFTPServer/Services/TFTPAppDialogsImpl.cs
@@ -2,6 +2,7 @@
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Platform.Storage;
+using Avalonia.Threading;
 using System;
 using System.ComponentModel;
 using System.Linq;
@@ -14,17 +15,29 @@ public class TFTPAppDialogsImpl(IViewResolver viewResolver) : ITFTPAppDialogs
 {
     public Task<UIConfigDialog.ChangeConfigResult> ShowUIConfigDialog(INotifyPropertyChanged vm, UISettings settings, string configPath)
     {
-        return UIConfigDialog.ShowDialog(viewResolver.LocateView(vm), settings, configPath);
+        // invoke with background priority, otherwise you'll get problems where the main window keeps the focus even though the dialog is visible
+        return Dispatcher.UIThread.InvokeAsync(
+            () => UIConfigDialog.ShowDialog(viewResolver.LocateView(vm),settings,configPath),
+            DispatcherPriority.Background
+        );
     }
 
     public Task<ConfigDialog.ChangeConfigResult> ShowConfigDialog(INotifyPropertyChanged vm, ServerSettings settings)
     {
-        return ConfigDialog.ShowDialog(viewResolver.LocateView(vm), settings);
+        // invoke with background priority, otherwise you'll get problems where the main window keeps the focus even though the dialog is visible
+        return Dispatcher.UIThread.InvokeAsync(
+            () => ConfigDialog.ShowDialog(viewResolver.LocateView(vm), settings),
+            DispatcherPriority.Background
+        );        
     }
 
     public Task ShowErrorDialog(INotifyPropertyChanged vm, string title, string header, string details)
     {
-        return ErrorDialog.ShowErrorDialog(viewResolver.LocateView(vm),title, header, details);
+        // invoke with background priority, otherwise you'll get problems where the main window keeps the focus even though the dialog is visible
+        return Dispatcher.UIThread.InvokeAsync(
+            () => ErrorDialog.ShowErrorDialog(viewResolver.LocateView(vm), title, header, details),
+            DispatcherPriority.Background
+        );
     }
 
     public async Task<string?> ShowFolderPicker(INotifyPropertyChanged vm, string title)
